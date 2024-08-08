2aae0c9f15212139c2e2c283e0de2775c639483c
hilde801 <165758371+hilde801@users.noreply.github.com>
2024-08-08 16:59:57 +0800

    This is good

--- HttpShare/Windows/MainWindow.xaml.cs
+++ HttpShare/Windows/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 using HttpShare.Models;
+using Microsoft.AspNetCore.Builder;
+using System.ComponentModel;
 using System.Windows;
 
 namespace HttpShare.Windows;
@@ -7,14 +9,38 @@ public partial class MainWindow : Window
 {
 	private MainWindowDataContext ParsedDataContext => (MainWindowDataContext)DataContext;
 
+
+	private WebApplication WebApplication { get; set; } = null!;
+
+
 	public MainWindow()
 	{
 		InitializeComponent();
 	}
 
 
-	private void OnClickServerToggleButton(object? sender, RoutedEventArgs _)
+	private async void OnClickServerToggleButton(object? sender, RoutedEventArgs _)
 	{
 		ParsedDataContext.IsServerRunning = !ParsedDataContext.IsServerRunning;
+
+		if (ParsedDataContext.IsServerRunning)
+		{
+			WebApplicationBuilder builder = WebApplication.CreateBuilder();
+
+			WebApplication = builder.Build();
+
+			// TODO For testing purposes only - remove this later
+			WebApplication.Map("/", () => "Hello, World!");
+
+			await WebApplication.StartAsync();
+		}
+
+		else await WebApplication.DisposeAsync();
+	}
+
+	protected async override void OnClosing(CancelEventArgs e)
+	{
+		if (ParsedDataContext.IsServerRunning) await WebApplication.DisposeAsync();
+		base.OnClosing(e);
 	}
 }
