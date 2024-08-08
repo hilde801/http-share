ae9fc7d49ce08f88a35592d717397813e08b488a
hilde801 <165758371+hilde801@users.noreply.github.com>
2024-08-08 15:08:26 +0800

    The outbox files list binding works now

--- HttpShare/Controls/OutboxControl.xaml.cs
+++ HttpShare/Controls/OutboxControl.xaml.cs
@@ -1,5 +1,7 @@
 using HttpShare.Models;
 using Microsoft.Win32;
+using System.ComponentModel;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -16,18 +18,39 @@ public partial class OutboxControl : UserControl
 	}
 
 
+	private void OnFileOkAddFilesDialog(object? sender, CancelEventArgs _)
+	{
+		OpenFileDialog addFilesDialog = (OpenFileDialog)sender!;
+		Stream[] fileStreams = addFilesDialog.OpenFiles();
+
+		foreach (FileStream fileStream in fileStreams)
+		{
+			string path = fileStream.Name;
+			byte[] data = null!;
+
+			using (MemoryStream memoryStream = new MemoryStream())
+			{
+				fileStream.CopyTo(memoryStream);
+				data = memoryStream.ToArray();
+			}
+
+			fileStream.Dispose();
+
+			ParsedDataContext.OutboxFiles.Add(new File(path, data));
+		}
+	}
+
 	private void OnClickAddFilesButton(object? sender, RoutedEventArgs _)
 	{
-		OpenFileDialog openFileDialog = new OpenFileDialog
+		OpenFileDialog addFilesDialog = new OpenFileDialog
 		{
 			Multiselect = true,
 			CheckFileExists = true,
 			CheckPathExists = true
 		};
 
-		// TODO Add something here later
-
-		openFileDialog.ShowDialog();
+		addFilesDialog.FileOk += OnFileOkAddFilesDialog;
+		addFilesDialog.ShowDialog();
 	}
 
 	private void OnClickClearOutboxButton(object? sender, RoutedEventArgs _)
