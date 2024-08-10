425e8e1930c3fc846fb7de11e7ab321ab8a50e37
hilde801 <165758371+hilde801@users.noreply.github.com>
2024-08-10 21:50:43 +0800

    Added license stubs

--- HttpShare/Controls/OutboxControl.xaml.cs
+++ HttpShare/Controls/OutboxControl.xaml.cs
@@ -1,3 +1,8 @@
+// =============================================================================
+// Copyright 2024 Hilde801 (https://github.com/hilde801)
+// This file is a part of http-share
+// =============================================================================
+
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
