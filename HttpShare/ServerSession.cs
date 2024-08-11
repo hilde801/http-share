25f581617ba371250a3aad8924cb8d480655169b
hilde801 <165758371+hilde801@users.noreply.github.com>
2024-08-11 17:00:22 +0800

    Added comments to ServerSession

--- HttpShare/ServerSession.cs
+++ HttpShare/ServerSession.cs
@@ -5,7 +5,17 @@ using System;
 
 namespace HttpShare;
 
+/// <summary>
+/// This class is the base to describe server session classes.
+/// All classes that inherit this class will be used as a session type.
+/// </summary>
 public abstract class ServerSession
 {
+	/// <summary>
+	/// Gets the host's display name.
+	/// </summary>
+	/// <returns>
+	/// The host's display name in the "(hostname)/(username)" format.
+	/// </returns>
 	public string HostName => $"{Environment.MachineName}/{Environment.UserName}";
 }
