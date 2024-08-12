2e29509344c896ce4053d06ef1e77dd64dc6b782
hilde801 <165758371+hilde801@users.noreply.github.com>
2024-08-12 12:48:17 +0800

    Added comments to DualSession

--- HttpShare/DualSession.cs
+++ HttpShare/DualSession.cs
@@ -5,6 +5,9 @@ using System.Collections.Generic;
 
 namespace HttpShare;
 
+/// <summary>
+/// Describes a server session that supports both sending and receiving files. 
+/// </summary>
 public sealed class DualSession(ICollection<File> outboxFiles) : ServerSession,
 	ISendSession, IReceiveSession
 {
