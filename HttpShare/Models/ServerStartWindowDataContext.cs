4a7f901bdaa19be3e398dcfab47d18da5112b5a1
hilde801 <165758371+hilde801@users.noreply.github.com>
2024-08-14 01:40:48 +0800

    Added comments

--- HttpShare/Models/ServerStartWindowDataContext.cs
+++ HttpShare/Models/ServerStartWindowDataContext.cs
@@ -1,15 +1,27 @@
+// Copyright 2024 Hilde801 (https://github.com/hilde801)
+// This file is a part of http-share
+
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace HttpShare.Models;
 
+/// <summary>
+/// The data context class for <see cref="Windows.ServerStartWindow" />
+/// </summary>
 public sealed class ServerStartWindowDataContext : INotifyPropertyChanged
 {
 	public event PropertyChangedEventHandler? PropertyChanged;
 
 
+	/// <summary>
+	/// The address of the server.
+	/// </summary>
 	private string serverAddress = string.Empty;
 
+	/// <summary>
+	/// The address of the server.
+	/// </summary>
 	public string ServerAddress
 	{
 		get => serverAddress;
@@ -22,6 +34,10 @@ public sealed class ServerStartWindowDataContext : INotifyPropertyChanged
 	}
 
 
+	/// <summary>
+	/// Invokes <see cref="PropertyChanged"/>.
+	/// </summary>
+	/// <param name="propertyName">The name of the invoking property.</param>
 	private void InvokePropertyChangedEvent([CallerMemberName] string? propertyName = null)
 	{
 		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
--- HttpShare/Windows/ServerStartWindow.xaml.cs
+++ HttpShare/Windows/ServerStartWindow.xaml.cs
@@ -1,3 +1,6 @@
+// Copyright 2024 Hilde801 (https://github.com/hilde801)
+// This file is a part of http-share
+
 using System;
 using System.Linq;
 using System.Net;
@@ -7,17 +10,29 @@ using HttpShare.Models;
 
 namespace HttpShare.Windows;
 
+/// <summary>
+/// The code-behind class for the server start notification dialog.
+/// </summary>
 public partial class ServerStartWindow : Window
 {
+	/// <summary>
+	/// The data context object parsed to <see cref="ServerStartWindowDataContext"/>.
+	/// </summary>
 	private ServerStartWindowDataContext ParsedDataContext => (ServerStartWindowDataContext) DataContext;
 
 
+	/// <summary>
+	/// The class constructor.
+	/// </summary>
 	public ServerStartWindow()
 	{
 		InitializeComponent();
 	}
 
 
+	/// <summary>
+	/// Overrides the OnInitialized method.
+	/// </summary>
 	protected override void OnInitialized(EventArgs e)
 	{
 		ParsedDataContext.ServerAddress = GetServerAddress();
@@ -26,23 +41,40 @@ public partial class ServerStartWindow : Window
 	}
 
 
+	/// <summary>
+	/// Handles the "OK" button on click event.
+	/// </summary>
 	private void OnClickOkButton(object? sender, RoutedEventArgs _)
 	{
 		Close();
 	}
 
+
+	/// <summary>
+	/// Handles the "Copy to Clipboard" button on click event.
+	/// </summary>
 	private void OnClickCopyToClipboardButton(object? sender, RoutedEventArgs _)
 	{
 		Clipboard.SetText(ParsedDataContext.ServerAddress);
 	}
 
 
+	/// <summary>
+	/// Checks whether if an <see cref="IPAddress"/> is a local address or not.
+	/// This function will check if the address starts with "192.168" in accordance to RFC1918.
+	/// </summary>
+	/// <param name="address">The <see cref="IPAddress"/> to check.</param>
+	/// <returns>Returns true if the input is a local address.</returns>
 	private static bool CheckIfLocalAddress(IPAddress address)
 	{
 		byte[] bytes = address.GetAddressBytes();
 		return bytes[0] == 192 && bytes[1] == 168;
 	}
 
+	/// <summary>
+	/// Gets all local <see cref="IPAddress"/>es.
+	/// </summary>
+	/// <returns>The URI string of the first local address fetched.</returns>
 	public static string GetServerAddress()
 	{
 		IPAddress[] ipAddresses = Dns.GetHostAddresses(Dns.GetHostName())
