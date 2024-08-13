3a926a2ab5d31d628c46d5c358c61e1fd0c0ac87
hilde801 <165758371+hilde801@users.noreply.github.com>
2024-08-13 18:41:40 +0800

    This is good

--- HttpShare/Models/ServerStartWindowDataContext.cs
+++ HttpShare/Models/ServerStartWindowDataContext.cs
@@ -1,8 +1,29 @@
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace HttpShare.Models;
 
 public sealed class ServerStartWindowDataContext : INotifyPropertyChanged
 {
 	public event PropertyChangedEventHandler? PropertyChanged;
+
+
+	private string serverAddress = string.Empty;
+
+	public string ServerAddress
+	{
+		get => serverAddress;
+
+		set
+		{
+			serverAddress = value;
+			InvokePropertyChangedEvent();
+		}
+	}
+
+
+	private void InvokePropertyChangedEvent([CallerMemberName] string? propertyName = null)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
 }
--- HttpShare/Windows/ServerStartWindow.xaml.cs
+++ HttpShare/Windows/ServerStartWindow.xaml.cs
@@ -1,11 +1,43 @@
+using System;
+using System.Linq;
+using System.Net;
 using System.Windows;
+
+using HttpShare.Models;
 
 namespace HttpShare.Windows;
 
 public partial class ServerStartWindow : Window
 {
+	private ServerStartWindowDataContext ParsedDataContext => (ServerStartWindowDataContext) DataContext;
+
+
 	public ServerStartWindow()
 	{
 		InitializeComponent();
+	}
+
+
+	protected override void OnInitialized(EventArgs e)
+	{
+		ParsedDataContext.ServerAddress = GetServerAddress();
+
+		base.OnInitialized(e);
+	}
+
+
+	private static bool CheckIfLocalAddress(IPAddress address)
+	{
+		byte[] bytes = address.GetAddressBytes();
+		return bytes[0] == 192 && bytes[1] == 168;
+	}
+
+	public static string GetServerAddress()
+	{
+		IPAddress[] ipAddresses = Dns.GetHostAddresses(Dns.GetHostName())
+			.Where(CheckIfLocalAddress)
+			.ToArray();
+
+		return $"http://{ipAddresses[0]}:80";
 	}
 }
