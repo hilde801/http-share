50e3c69902c85554162fe10358ced2e0843b1966
hilde801 <165758371+hilde801@users.noreply.github.com>
2024-08-13 17:28:22 +0800

    Created and implemented ServerStartWindowDataContext

---
+++ HttpShare/Models/ServerStartWindowDataContext.cs
@@ -0,0 +1,8 @@
+using System.ComponentModel;
+
+namespace HttpShare.Models;
+
+public sealed class ServerStartWindowDataContext : INotifyPropertyChanged
+{
+	public event PropertyChangedEventHandler? PropertyChanged;
+}
