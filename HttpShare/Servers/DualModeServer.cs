23fdcca6d3dfdae62157730e93cbd660f01a374f
hilde801 <165758371+hilde801@users.noreply.github.com>
2024-08-27 22:45:58 +0800

    Some code cleanup

--- HttpShare/Servers/DualModeServer.cs
+++ HttpShare/Servers/DualModeServer.cs
@@ -3,7 +3,6 @@ using HttpShare.Sessions;
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace HttpShare.Servers;
 
