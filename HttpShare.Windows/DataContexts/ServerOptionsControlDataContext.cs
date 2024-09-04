d10730c0fa65eeaf9071455d81229583912444fe
hilde801 <165758371+hilde801@users.noreply.github.com>
2024-09-05 00:24:47 +0800

    Added binding for port input

--- HttpShare.Windows/DataContexts/ServerOptionsControlDataContext.cs
+++ HttpShare.Windows/DataContexts/ServerOptionsControlDataContext.cs
@@ -1,8 +1,37 @@
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 
 namespace HttpShare.Windows.DataContexts;
 
-public sealed class ServerOptionsControlDataContext : INotifyPropertyChanged
+public sealed class ServerOptionsControlDataContext : INotifyPropertyChanged, IDataErrorInfo
 {
 	public event PropertyChangedEventHandler? PropertyChanged;
+
+
+	public string Error => errors.Count == 0 ? string.Empty : errors.First().Value;
+
+	public string this[string columnName]
+	{
+		get => errors.Where(pair => pair.Key == columnName)
+			.Select(pair => pair.Value).FirstOrDefault(string.Empty);
+	}
+
+
+	private string portInput = "80";
+
+	private int port = 80;
+
+	public int Port => port;
+
+	public string PortInput
+	{
+		get => portInput;
+
+		set
+		{
+			portInput = value;
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+		}
+	}
 }
