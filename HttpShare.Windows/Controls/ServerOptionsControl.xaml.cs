866732a6d7cad83aa866d5b6f348829ee4405110
hilde801 <165758371+hilde801@users.noreply.github.com>
2024-09-04 20:01:07 +0800

    Created ServerOptionsControl

---
+++ HttpShare.Windows/Controls/ServerOptionsControl.xaml.cs
@@ -0,0 +1,14 @@
+// Copyright 2024 Hilde801 (https://github.com/hilde801)
+// This file is a part of http-share
+
+using System.Windows.Controls;
+
+namespace HttpShare.Windows.Controls;
+
+public partial class ServerOptionsControl : UserControl
+{
+	public ServerOptionsControl()
+	{
+		InitializeComponent();
+	}
+}
