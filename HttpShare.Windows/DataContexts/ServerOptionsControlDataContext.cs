e0945ac9c78264a5a89685d6e9a638f4b686283c
hilde801 <165758371+hilde801@users.noreply.github.com>
2024-09-05 00:52:48 +0800

    Added error handling stuff

--- HttpShare.Windows/DataContexts/ServerOptionsControlDataContext.cs
+++ HttpShare.Windows/DataContexts/ServerOptionsControlDataContext.cs
@@ -9,6 +9,15 @@ public sealed class ServerOptionsControlDataContext : INotifyPropertyChanged, ID
 	public event PropertyChangedEventHandler? PropertyChanged;
 
 
+	private readonly ICollection<KeyValuePair<string, string>> errors = [];
+
+
+	public ServerOptionsControlDataContext()
+	{
+		PropertyChanged += ValidateInputs;
+	}
+
+
 	public string Error => errors.Count == 0 ? string.Empty : errors.First().Value;
 
 	public string this[string columnName]
@@ -34,4 +43,34 @@ public sealed class ServerOptionsControlDataContext : INotifyPropertyChanged, ID
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
 		}
 	}
+
+
+	private void ValidateInputs(object? sender, PropertyChangedEventArgs args)
+	{
+		if (!string.IsNullOrEmpty(args.PropertyName) || !string.IsNullOrWhiteSpace(args.PropertyName))
+			return;
+
+		errors.Clear();
+
+		if (!int.TryParse(portInput, out port))
+		{
+			string errorMessage = "The input must be a number.";
+			errors.Add(new KeyValuePair<string, string>(nameof(PortInput), errorMessage));
+		}
+
+		if (port is < 0 or > ushort.MaxValue)
+		{
+			string errorMessage = $"The input must be between 0 and {ushort.MaxValue}.";
+			errors.Add(new KeyValuePair<string, string>(nameof(PortInput), errorMessage));
+		}
+
+		// TODO Add something here later
+
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Errors)));
+	}
+
+
+	public record ErrorItem(string Input, string Message);
+
+	public IEnumerable<ErrorItem> Errors => errors.Select(item => new ErrorItem(item.Key, item.Value));
 }
