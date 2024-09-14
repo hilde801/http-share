6f0c95ea006b18c6ed28adb0b4a7eacd96457a0a
hilde801 <165758371+hilde801@users.noreply.github.com>
2024-09-14 08:27:56 +0800

    Some corrections

--- HttpShare/Models/LogInModel.cs
+++ HttpShare/Models/LogInModel.cs
@@ -2,6 +2,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace HttpShare.Models;
 
-public sealed record LogIn(
+public sealed record LogInModel(
 	[Required, DataType(DataType.Text)] string DisplayName
 );
