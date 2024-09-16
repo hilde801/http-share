3702cc2c3018c82e34afa7f2c20df471c592f077
hilde801 <165758371+hilde801@users.noreply.github.com>
2024-09-16 18:57:03 +0800

    Added LogControl

---
+++ HttpShare.Windows/Controls/LogControl.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace HttpShare.Windows.Controls;
+
+public partial class LogControl : UserControl
+{
+	public LogControl()
+	{
+		InitializeComponent();
+	}
+}
